4b2334db28ea26d2e9cdbf55c35b6e7719b2f627
Minh Trí Bùi <minhtribui16@gmail.com>
2023-04-19 15:05:26 +0700

    Done SearchView/GroupBy

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -5,6 +5,7 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char('Patient Name:')
+    ref = fields.Char('Reference')
     age = fields.Integer('Age')
     gender = fields.Selection(
         [('male','Male'),
