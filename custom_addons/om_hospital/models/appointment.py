ed9dd3c0825e09a0cf51c21e1c9d6005cdc1a166
Minh Trí Bùi <minhtribui16@gmail.com>
2023-04-24 16:51:37 +0700

    Done Btn/HelpMsg/RainbowOnClick/DecorationBadgeRecord

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -7,14 +7,35 @@ class HospitalPatient(models.Model):
     _description = "Hospital Appointment"
     _rec_name = 'ref'
 
-    patient_id = fields.Many2one(comodel_name='hospital.patient',string="Patient")
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
 
     ref = fields.Char('Reference')
     prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_cosultation', 'In Cosultation'),
+        ('done', 'Done'),
+        ('cancel', 'Canceled')], string="Status", default="draft")
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
+
+    def action_test(self):
+        print('Clicked Btn!!!')
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Click Successful',
+                'type': 'rainbow_man',
+            }
+        }
